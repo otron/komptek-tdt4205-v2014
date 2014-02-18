3f968738a8e4f74944716974c981726a3fdf23b1
Odd Magnus Trondrud <trondrud@ntnu.no>
2014-02-18 16:02:02 +0100

    *whistle sounds*, ps3 code. whoo.

--- 03/code/src/simplifynodes.c
+++ 03/code/src/simplifynodes.c
@@ -56,10 +56,26 @@ Node_t *simplify_list_with_null ( Node_t *root, int depth )
 
 Node_t *simplify_list ( Node_t *root, int depth )
 {
+	// okay so we just get a Node_t in here, right?
+	// yeah, of an appropriate type. Neat.
 	if(outputStage == 4)
 		fprintf ( stderr, "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
 		
+	Node_t* child;
+	for (int i = 0; i < (root->n_children); i++) {
+		child = root->children[i];
+		if (child->nodetype == root->nodetype) {
+			// first we recursively simplify
+			child = child->simplify(child, depth+1);
+			// THEN we steal the children
+			for (int j = 0; j < child->n_children; j++) {
+				child->children[j]->
+
+			}
+		}
+	}
 }
+
 
 
 Node_t *simplify_expression ( Node_t *root, int depth )
