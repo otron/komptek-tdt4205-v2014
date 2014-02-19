08ddc1d5655845e67c9c6aa5d7ba2712dac6e0dc
Odd Magnus Trondrud <trondrud@ntnu.no>
2014-02-19 20:26:35 +0100

    touched bindnames.c a bit

--- 03/code/src/bindnames.c
+++ 03/code/src/bindnames.c
@@ -4,15 +4,22 @@ char* thisClass;
 
 int bind_default ( node_t *root, int stackOffset)
 {
+	if (root->children != NULL) {
+		for (int i = 0; i < root->n_children; i++) {
+			if (root->children[i] != NULL)
+				root->children[i]->bind_names(root->children[i], stackOffset);
+		}
+
+	}
 	
 }
 
 int bind_constant ( node_t *root, int stackOffset)
 {
-
 	if(outputStage == 6)
 		fprintf ( stderr, "CONSTANT\n");
 	
+	// do something with the string constant
 }
 
 
