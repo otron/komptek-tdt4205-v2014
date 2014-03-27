e19d11277063cea2001acf76b0752d747ad7712e
Odd Magnus Trondrud <trondrud@ntnu.no>
2014-03-27 14:03:45 +0100

    does the string table work now? idk

--- 03/code/src/symtab.c
+++ 03/code/src/symtab.c
@@ -5,30 +5,61 @@
 extern int arch;
 extern int outputStage; 
 
-static char **strings;
-static int strings_size = 16, strings_index = -1;
+static char **strings; // the array of strings
+static int strings_size = 16, //this is the number of strings the array can fit, right? yeah let's go with that
+		   strings_index = -1; //number of strings in the array. Wait, what? I think you mean "number of strings in the array -1"
+// oh, no you don't, you've just set it to -1 instead of 0 because of reasons, haven't you?
+// sheesh
 
 
 void symtab_init ( void )
 {
-    
+	// should perform any needed initialization
+	// i'm just going to have this double strings_size and malloc a new char** and then copy the contents of strings over to it
+	// and then free the old strings
+	// and then set new_strings to strings
+
+	strings_size *= 2; //double the space!
+	char** new_strings = (char**) malloc(sizeof(char*) * strings_size);
+	for (int i = 0; i < strings_index; i++) {
+		new_strings[i] = strings[i];
+		strings[i] = NULL;
+	}
+	free(strings);
+	strings = new_strings;
+
+	return 0;
 }
 
 
 void symtab_finalize ( void )
 {
-    
+	// should free any memory allocated for the table and its contents
+	// make sure you avoid double freeing anything
+
+	for (int i = 0; i < strings_index; i++) {
+		free(strings[i]);
+	}
+	free(strings);
+	return 0;
 }
 
 
 int strings_add ( char *str )
 {
 
+	// should add a string to the table and return the index
 
     if(outputStage == 7)
         fprintf ( stderr, "Add strings (%s), index: %d \n", str, strings_index );
 
-    
+	if (strings_index == strings_size) {
+		symtab_init(); //increase the size of the array
+	}
+
+	strings[strings_index] = str;
+	strings_index++;
+	return (strings_index - 1);
 }
 
 // Prints the data segment of the assembly code
